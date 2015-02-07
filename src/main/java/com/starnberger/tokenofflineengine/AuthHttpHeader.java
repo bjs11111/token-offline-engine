7e1469ad180c371886377bae87dc0d4a9b9997dd
protoss78 <protoss78@protoss78-W230SS>
2015-02-07 12:37:00 +0100

    Added user role to http header constants

--- src/main/java/com/starnberger/tokenofflineengine/AuthHttpHeader.java
+++ src/main/java/com/starnberger/tokenofflineengine/AuthHttpHeader.java
@@ -3,4 +3,5 @@ package com.starnberger.tokenofflineengine;
 public class AuthHttpHeader {
 	public static final String SERVICE_KEY = "service_key";
 	public static final String AUTH_TOKEN = "auth_token";
+	public static final String USER_ROLE = "user_role";
 }
